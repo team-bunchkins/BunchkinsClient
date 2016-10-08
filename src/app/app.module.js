fd64b638b2dcccbfa855441ff824ae82c2299d14
Vincent Duong <vduong178@gmail.com>
2016-10-07 22:48:12 -0700

    Rough draft of game ui

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -3,6 +3,15 @@
 
     angular
         .module('app', [
-            
-        ]);
+        	'ui.router'
+        ])
+        .value('apiUrl', 'http://localhost:88888/api')
+        .config(appConfig);
+
+    appConfig.$inject = ['$urlRouterProvider', '$stateProvider'];
+
+    function appConfig($urlRouterProvider, $stateProvider) {
+
+    	$urlRouterProvider.otherwise('/');
+    }
 })();
