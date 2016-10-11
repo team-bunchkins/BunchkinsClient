0d55e7733fd27740a63b1207d572a4cd5a51aeaa
Your Name <christiannaga1@gmail.com>
2016-10-11 16:35:18 -0700

    Added template for game functions

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -5,21 +5,49 @@
         .module('app')
         .controller('GameController', GameController);
 
-    GameController.$inject = ['bunchkinsFactory'];
+    GameController.$inject = ['bunchkinsFactory', '$scope', '$state'];
 
     /* @ngInject */
-    function GameController(bunchkinsFactory) {
+    function GameController(bunchkinsFactory, $scope, $state) {
         var vm = this;
         vm.title = 'GameController';
         vm.game = bunchkinsFactory.game;
         vm.player = bunchkinsFactory.player;
         vm.players = bunchkinsFactory.opponents;
+        
+        //functions
+        vm.proceed = proceed;
+        vm.fight = fight;
+        vm.run = run;
+        vm.pass = pass;
+        vm.playCard = playCard;
 
         activate();
 
         ////////////////
 
         function activate() {
+          
+        }
+        
+        function proceed() {
+            bunchkinsFactory.proceed();
+        } 
+    
+        function fight() {
+            bunchkinsFactory.fight();
+        }
+    
+        function run() {
+            bunchkinsFactory.run();
+        }
+    
+        function pass() {
+            bunchkinsFactory.pass();
+        }
+    
+        function playCard() {
+            bunchkinsFactory.playCard();
         }
     }
 })();
